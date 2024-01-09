b99a82f41c26c7cc708b9522790e530fa3b687c3
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-01-09 20:13:39 +0300

    fix: fix battleship loop

--- Part_II/ship_battle/battle_ships.js
+++ Part_II/ship_battle/battle_ships.js
@@ -1,6 +1,6 @@
 var field = 8;
 var shipLenght = 3;
-var randomLocation = Math.floor(Math.random()*(field-shipLenght));
+var randomLocation = Math.floor(Math.random()*(field-shipLenght-1));
 var location1 = randomLocation + 1;
 var location2 = location1 + 1;
 var location3 = location1 + 2;
@@ -12,6 +12,7 @@ var isSink = false;
 
 while (isSink == false) {
   userQuery = prompt("Fire into enemy ship! write a number from 1 to " + field);
+  console.log(isSink);
     if ( userQuery < 0 || userQuery > field){
       alert (" Please put a valid number");
     } else {
@@ -19,8 +20,9 @@ while (isSink == false) {
       if ( userQuery == location1 || userQuery == location2 || userQuery == location3) {
         hits = hits + 1;
         alert ("HIT! You hit " + hits + " of " + shipLenght + " Its a " + guess + " of you shoot.");
+        
         if ( hits == shipLenght){
-          isSink == true;
+          isSink = true;
           alert ( "WIN! You make " + guess + " shoots form enamy ship");
         }
       } else {
