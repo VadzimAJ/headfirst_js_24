25f2d0ce833b5560cd248769a0cd7bad168feb42
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-01-11 06:39:20 +0300

    mend
     feat: rewrote strange factorial code and run it

---
+++ Part_IV_Array/Slogan_generator/index.js
@@ -0,0 +1,15 @@
+function sloganGenerator () {
+  var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win"];
+  var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
+  var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+
+  var random1 = Math.floor(Math.random() * words1.length);
+  var random2 = Math.floor(Math.random() * words2.length);
+  var random3 = Math.floor(Math.random() * words3.length);
+
+  var sloganPhrase =  words1[random1] + " " + words2[random2] + " " + words3[random3];
+  alert(sloganPhrase);
+
+}
+
+sloganGenerator();
