bb28af6b3213d8e260d1328e96825c2b02791d9f
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-01-13 09:45:42 +0300

    feat: comlite refactor bubble code into funtion paradigme

--- Part_IV_Array/Best_bubble/index.js
+++ Part_IV_Array/Best_bubble/index.js
@@ -1,29 +1,37 @@
-var bubbleScores = [60, 50, 60, 60, 60, 60, 69, 60, 58, 54, 60, 54, 58, 50, 52, 54, 48, 69, 46, 31, 57, 25, 44, 18, 41, 69, 11, 53, 55, 61, 51, 44];
+var bubbleScores = [60, 50, 60, 58, 54, 54,
+  58, 50, 52, 54, 48, 69,
+  34, 55, 51, 52, 44, 51,
+  69, 64, 66, 55, 52, 61,
+  46, 31, 57, 52, 44, 18,
+  41, 53, 55, 61, 51, 44];
 
 function bubbleGrade() {
   var scoreCounter = 0;
   var arrLength = bubbleScores.length - 1;
   var bestSolution = 0;
   var bestSolutionPositions = [];
-  
+  console.log ( "Начало обработки массива bubbleScores содержащего " + bubbleScores.length + " элемента")
   while (arrLength >= scoreCounter) {
     document.write("Bubble solution #" + (scoreCounter) + " score: " + bubbleScores[scoreCounter] + "<br>");
     if (bubbleScores[scoreCounter] > bestSolution) {
-      console.log("В масииве bubbleScores под индексом " + scoreCounter +" содержится число " + bubbleScores[scoreCounter] + " оно больше чем  bestSolution которое содержит " + bestSolution );
+        console.log("В масииве bubbleScores под индексом " + scoreCounter +" содержится число " + bubbleScores[scoreCounter] + " оно больше чем  bestSolution которое содержит " + bestSolution );
       bestSolution = bubbleScores[scoreCounter];
-      console.log("bestSolution принимает в себя значение из bubbleScores[scoreCounter] и теперь равно " + bubbleScores[scoreCounter] + ". При этом массив полностью перезатирается");
+        console.log("Массив bestSolutionPositions перезатирается и принимает в себя значение "  + bubbleScores[scoreCounter] + " из bubbleScores[" + scoreCounter + "]");
       bestSolutionPositions = [scoreCounter];
-      console.log("Теперь в массиве bestSolutionPositions " + bestSolutionPositions.length + " ячеек которые содержат число " + bubbleScores[scoreCounter]);
+        console.log("Теперь в массиве bestSolutionPositions " + bestSolutionPositions.length + " ячеек которые указывают на число " + bubbleScores[scoreCounter] + " из массива bubbleScores.");
     } else if (bubbleScores[scoreCounter] === bestSolution) {
       bestSolutionPositions.push(scoreCounter);
-      console.log("Индекс массива bubbleGrade " + scoreCounter + " содержит число " + bubbleScores[scoreCounter] + " он пошится в bestSolutionPositions. Теперь в bestSolutionPositions содержатся индексы [" + bestSolutionPositions +"]");
+        console.log("Индекс массива bubbleGrade " + scoreCounter + " содержит число " + bubbleScores[scoreCounter] + ". Этот индекс записывается в массив bestSolutionPositions. Теперь в bestSolutionPositions содержатся индексы [" + bestSolutionPositions +"]");
+    } else {
+        console.log("Число в индексе " + scoreCounter + " меньше bestSolution переходим к увеличению счетчика.");
     }
 
     scoreCounter = scoreCounter + 1;
-    console.log("scoreCounter увеличился на единицу и переходит к индексу массива bubbleScores " + scoreCounter);
+      console.log("Счетчик scoreCounter увеличился на единицу и переходит к индексу массива bubbleScores " + scoreCounter);
   }
+    console.log ( "Обработка массива bubbleScores завершена. На страницу выводятся результаты")
 
-  document.write("<br> Bubble test: " + bubbleScores.length);
+  document.write("<br> Bubble tests: " + bubbleScores.length);
   document.write("<br> Highest bubble solution score: " + bestSolution);
   document.write("<br> Solution(s) with the highest score: " + bestSolutionPositions.join(', '));
 }
---
+++ Part_IV_Array/Best_bubble_functions/index.js
@@ -0,0 +1,59 @@
+var bubbleScores = [60, 50, 60, 58, 54, 54,
+  58, 50, 52, 54, 48, 69,
+  34, 55, 51, 52, 44, 51,
+  69, 64, 66, 55, 52, 61,
+  46, 31, 57, 52, 44, 18,
+  41, 53, 55, 61, 51, 44];
+
+var costs = [.25, .27, .25, .25, .25, .25, .33, .31, 
+    .25, .29, .27, .22, .31, .25, .25, .33, 
+    .21, .25, .25, .25, .28, .25, .24, .22,
+    .20, .25, .30, .25, .24, .25, .25, .25, 
+    .27, .25, .26, .29];
+
+
+var bestSolution = 0;
+var bestSolutionPositions = [];
+var arrLength = bubbleScores.length - 1;
+
+function bubbleGrade(scoreArr) {
+  
+  for (var scoreCounter = 0; arrLength > scoreCounter; scoreCounter++) {
+    document.write("Bubble solution #" + (scoreCounter) + " score: " + scoreArr[scoreCounter] + "<br>");
+      if (scoreArr[scoreCounter] > bestSolution) {
+        bestSolution = scoreArr[scoreCounter];
+        bestSolutionPositions = [scoreCounter];
+      } else if (scoreArr[scoreCounter] == bestSolution){
+        bestSolutionPositions.push(scoreCounter);
+      }
+    }
+  // while (arrLength >= scoreCounter) {
+  //   document.write("Bubble solution #" + (scoreCounter) + " score: " + bubbleScores[scoreCounter] + "<br>");
+  //   if (bubbleScores[scoreCounter] > bestSolution) {
+  //     bestSolution = bubbleScores[scoreCounter];
+  //     bestSolutionPositions = [scoreCounter];
+  //   } else if (bubbleScores[scoreCounter] === bestSolution) {
+  //     bestSolutionPositions.push(scoreCounter);
+  //   }
+
+  //   scoreCounter = scoreCounter + 1;
+  // }
+
+  document.write("<br> Bubble tests: " + bubbleScores.length);
+  document.write("<br> Highest bubble solution score: " + bestSolution);
+  document.write("<br> Solution(s) with the highest score: " + bestSolutionPositions.join(', '));
+  return (bestSolutionPositions);
+}
+
+function mostCostEffective (bubbleScores, costsArr, bestArr) {
+  var starCost = bestArr[0];
+  for ( var i = 0; bestArr > i; i++){
+    if (costsArr[bestArr[i]] > starCost) {
+      starCost = bubbleScores[bestArr[i]];
+    }
+  }
+  document.write("<br> Best cost on position : " + starCost + ". " + costsArr[bestArr[i]] + "is the most effective cost.");
+}
+
+bubbleGrade (bubbleScores);
+mostCostEffective (bubbleScores, costs, bestSolutionPositions );
