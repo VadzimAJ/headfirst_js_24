6e461caeeaa5a8a3a09d3c490b1fb3e054090de7
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-01-13 10:44:53 +0300

    fix: fix cost hendler

--- Part_IV_Array/Best_bubble_functions/index.js
+++ Part_IV_Array/Best_bubble_functions/index.js
@@ -1,12 +1,12 @@
 var bubbleScores = [60, 50, 60, 58, 54, 54,
-  58, 50, 52, 54, 48, 69,
+  58, 50, 52, 54, 48, 69, 69,
   34, 55, 51, 52, 44, 51,
   69, 64, 66, 55, 52, 61,
   46, 31, 57, 52, 44, 18,
   41, 53, 55, 61, 51, 44];
 
 var costs = [.25, .27, .25, .25, .25, .25, .33, .31, 
-    .25, .29, .27, .22, .31, .25, .25, .33, 
+    .25, .29, .27, .22, .21, .31, .25, .25, .33, 
     .21, .25, .25, .25, .28, .25, .24, .22,
     .20, .25, .30, .25, .24, .25, .25, .25, 
     .27, .25, .26, .29];
@@ -46,13 +46,18 @@ function bubbleGrade(scoreArr) {
 }
 
 function mostCostEffective (bubbleScores, costsArr, bestArr) {
-  var starCost = bestArr[0];
-  for ( var i = 0; bestArr > i; i++){
-    if (costsArr[bestArr[i]] > starCost) {
-      starCost = bubbleScores[bestArr[i]];
+  var bestCost = costsArr[bestArr[0]];
+  var bestCostPosition = bestArr[0];
+  for ( var i = 0; bestArr.length > i; i++){
+    console.log(costsArr[bestArr[i]]);
+    console.log(bestCost);
+    if (costsArr[bestArr[i]] <bestCost) {
+      bestCost = costsArr[bestArr[i]];
+      bestCostPosition = bestArr[i];
+      console.log(bestArr.length);
     }
   }
-  document.write("<br> Best cost on position : " + starCost + ". " + costsArr[bestArr[i]] + "is the most effective cost.");
+  document.write("<br> Best cost on position : " + bestCostPosition + ". "  + " The most effective cost is " + bestCost);
 }
 
 bubbleGrade (bubbleScores);
