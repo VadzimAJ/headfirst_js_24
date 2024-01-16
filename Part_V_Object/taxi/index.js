986af635265ea6d2af64579200cf3c6dd71d1182
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-01-16 09:54:41 +0300

    feat: add new cars into project

--- Part_V_Object/taxi/index.js
+++ Part_V_Object/taxi/index.js
@@ -8,6 +8,36 @@ var taxi = {
   mileage: 291341
 };
 
+var cadi = {
+  make: "GM",
+  model: "Caddilac",
+  yar: 1955,
+  color: "yellow",
+  passagers: 4,
+  convertable: false,
+  mileage: 12829
+};
+
+var fiat = {
+  make: "Fiat",
+  model: "500",
+  yar: 1955,
+  color: "yellow",
+  passagers: 4,
+  convertable: false,
+  mileage: 88000
+};
+
+var chevy = {
+  make: "Chevy",
+  model: "Bel Air",
+  yar: 1957,
+  color: "yellow",
+  passagers: 4,
+  convertable: false,
+  mileage: 1021
+};
+
 function prequal(car) {
   if (car.mileage > 10000) {
     return false;
@@ -19,6 +49,33 @@ function prequal(car) {
 }
 
 var worthLook = prequal (taxi);
+
+if (worthLook) {
+  console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+  console.log ( "You shold really pass on the " + taxi.make + " " + taxi.model);
+}
+
+
+var worthLook = prequal (cadi);
+
+if (worthLook) {
+  console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+  console.log ( "You shold really pass on the " + taxi.make + " " + taxi.model);
+}
+
+
+var worthLook = prequal (fiat);
+
+if (worthLook) {
+  console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+  console.log ( "You shold really pass on the " + taxi.make + " " + taxi.model);
+}
+
+
+var worthLook = prequal (chevy);
 
 if (worthLook) {
   console.log("You gotta check out this " + taxi.make + " " + taxi.model);
