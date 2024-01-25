401d394a11ef55f89043b47135dfe2f2a1c3f613
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-01-25 18:57:29 +0300

    feat: add code into funtion init

--- Part_VI_DOM/planet_dom/index.js
+++ Part_VI_DOM/planet_dom/index.js
@@ -1,3 +1,7 @@
-var planet = document.getElementById("green");
-planet.innerHTML = "Red Alert!";
-planet.setAttribute ("class" , "redtext");
+function init () {
+  var planet = document.getElementById("green");
+  planet.innerHTML = "Red Alert!";
+  planet.setAttribute ("class" , "redtext");
+}
+
+window.onload = init;
