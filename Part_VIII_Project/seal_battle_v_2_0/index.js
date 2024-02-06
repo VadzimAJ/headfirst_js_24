c162e3ed61d5e30592c6de08e055c6b061e15207
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-02-06 11:24:16 +0300

    feat: create logic of displaying ships and miss points

--- Part_VIII_Project/seal_battle_v_2_0/index.js
+++ Part_VIII_Project/seal_battle_v_2_0/index.js
@@ -1,1 +1,18 @@
-console.log("hello!");
+var view = {
+  displayMessage: function (msq) {
+    var messageArea = document.getElementById("massageArea");
+    messageArea.innerHTML = msq;
+  },
+  displayHit: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+}
+
+var ships = [{ locations: ['10', '20', '30'], hits: ['', '', ''] },
+              { locations: ['32', '33', '34'], hits: ['', '', ''] },
+              { locations: ['63', '64', '65'], hits: ['', '', 'hit'] }];
