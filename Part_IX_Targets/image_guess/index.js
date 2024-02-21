360a618d3699814ec875ab74f83ab40a8a4980ca
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-02-21 18:39:59 +0300

    feat: creat universal target handler

--- Part_IX_Targets/image_guess/index.js
+++ Part_IX_Targets/image_guess/index.js
@@ -1,12 +1,24 @@
 window.onload = init;
 
 function init() {
-  var image = document.getElementById('zero');
-  image.onclick = showAnswer;
-  
-}
+//   var image = document.getElementById('zero');
+  // image.onclick = showAnswer;
 
-function showAnswer () {
-  var image = document.getElementById('zero');
-  image.src = "./assets/zero.jpg"
+  var images = document.getElementsByTagName("img");
+  for (var i =0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+    console.log(images[i] + " add click ivent ")
+  };
+  console.log(images);
+  
+};
+
+function showAnswer (eventObj) {
+//   var image = document.getElementById('zero');
+  // image.src = "./assets/zero.jpg"
+
+  var image = eventObj.target;
+  var name = image.id;
+  name = './assets/' + name + '.jpg';
+  image.src = name;
 }
